2f97fe789c10fa5300f353d1f46f6e1637242d7b
Botond Sandor Kirei <botond.kirei@bel.utcluj.ro>
2019-11-13 18:31:19 +0200

    saved tcl

---
+++ Curs_3_Minimizarea_logica/Quine_McCluskey/vivado/proiect.tcl
@@ -0,0 +1,7 @@
+create_project proj
+set_property target_language VHDL [current_project]
+set_property simulator_language VHDL [current_project]
+add_files -norecurse {C:/Users/Botix/Desktop/Suport_HDL/Curs_3_Minimizarea_logica/Quine_McCluskey/src/test_list.vhd C:/Users/Botix/Desktop/Suport_HDL/Curs_3_Minimizarea_logica/Quine_McCluskey/src/quine_mccluskey.vhd C:/Users/Botix/Desktop/Suport_HDL/Curs_3_Minimizarea_logica/Quine_McCluskey/src/list_pkg.vhd C:/Users/Botix/Desktop/Suport_HDL/Curs_3_Minimizarea_logica/Quine_McCluskey/src/testbench.vhd}
+update_compile_order -fileset sources_1
+update_compile_order -fileset sources_1
+
